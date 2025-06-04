713c188b168a6a4fbac08c06eaeda5178f1babeb
Aibek Ramazanov <aibekramazanov@Aibeks-MacBook-Pro.local>
2025-06-04 13:21:10 +0500

    second commit

---
+++ compactObject.js
@@ -0,0 +1,28 @@
+var compactObject = function (obj) {
+    function dfc(obj) {
+      if (!obj) return false;
+      if (typeof obj !== "object") return obj;
+      if (Array.isArray(obj)) {
+        const newArray = [];
+        for (let i = 0; i < obj.length; i++) {
+          const curr = obj[i];
+          const subRes = dfc(curr));
+          if (subRes) {
+            newArray.push(subRes);
+          }
+        }
+        return newArray;
+      }
+      const newObj = {};
+      for (const key in obj) {
+        const subRes = dfc(obj[key]);
+        if (subRes) {
+          newObj[key] = subRes;
+        }
+      }
+      return newObj;
+    }
+  
+    return dfc(obj);
+  };
+  
